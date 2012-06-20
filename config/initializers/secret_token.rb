1e035ab34185c148b9b6fd743f93f2b090fdafb3
Dmitry Romanov <dmitry.romanov85@gmail.com>
2012-06-20 12:59:24 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEOAnalyzer::Application.config.secret_token = '8a51649fb8ffd430737708341de5b11f74c15602f876a6da04521d7e99d00c8783ea8f93c1c5f0831a04197fc43fb6a01196b4d638a1af2a66fdfa0db96f32f4'
