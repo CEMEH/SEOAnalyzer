d4e0b015932bd6017daacc6e991703b0ee97ce7a
Dmitry Romanov <dmitry.romanov85@gmail.com>
2012-07-29 22:09:27 +0700

    результат анализа в виде облака

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,18 +1,7 @@
 module SeoHelper
-  def word_weight(word)
-    weight = word.weight
-    result = weight.to_s + '%<br/>'
 
-    #@todo: вынести в модуль seo. определять основное контекстное ядро
-    if weight >= 2 and weight <= 7
-      result += '<span class="label label-success">OK</span>'
-    end
-    if weight > 7
-      result += '<span class="label label-important">SPAM</span>'
-    end
-    if weight < 2
-      result += '<span class="label label-warning">недостаточно</span>'
-    end
-    result
+  def wordmark_fontsize(word)
+    mark = word.mark
+    (mark * 0.6).to_i
   end
 end
